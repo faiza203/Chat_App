105348eba160fcd01522bc6566a1afeffddf14eb
faiza203 <fiazfaiza981@gmail.com>
2021-02-23 15:52:14 -0500

    Render index.hbs

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const app = require("express")();
 let http = require("http").Server(app);
 
 const port = process.env.PORT || 8000;
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 
-app.get("/" , (req,res) => {
-    res.send("Hello From Home")
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 http.listen(port, () => {
   console.log(`Text Chat App in running on ${port}`);
