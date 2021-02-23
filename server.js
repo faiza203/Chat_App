1fc940a91dceef81260fef53bdb9444ad38adf9e
faiza203 <fiazfaiza981@gmail.com>
2021-02-23 15:40:33 -0500

    Create index.js

--- server.js
+++ server.js
@@ -1,8 +1,1 @@
-const express = require("express");
-const socket = require("socket.io");
-const http = require("http");
-const cors = require("cors");
-
-const app = express();
-const server = http.createServer(app);
-const io = socket(server);
+console.log("Hello");
